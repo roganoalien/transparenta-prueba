0f6945d6c3759e9e34125fde5a3bf6c45f75b867
roganoalien <hola@rodrigogarcia.com.mx>
2020-10-18 19:27:48 -0500

    :fire: initial config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import WebFont from 'webfontloader';
+import ScrollTop from './scrollTop';
+import './tailwind.output.css';
+
+WebFont.load({
+	google: {
+		families: ['Open Sans:300,400,700,800', 'sans-serif']
+	}
+});
+
+const history = '';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<BrowserRouter history={history}>
+		<ScrollTop>
+			<App />
+		</ScrollTop>
+	</BrowserRouter>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
