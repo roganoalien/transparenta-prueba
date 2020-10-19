218d1e9d27ff33badb8b0592228746f8f4031bb5
roganoalien <hola@rodrigogarcia.com.mx>
2020-10-18 19:35:46 -0500

    :zap: config needed by default

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route, useLocation, Redirect } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { AnimatePresence } from 'framer-motion';
+/*
+██╗      █████╗ ██╗   ██╗ ██████╗ ██╗   ██╗████████╗       ██╗   
+██║     ██╔══██╗╚██╗ ██╔╝██╔═══██╗██║   ██║╚══██╔══╝       ██║   
+██║     ███████║ ╚████╔╝ ██║   ██║██║   ██║   ██║       ████████╗
+██║     ██╔══██║  ╚██╔╝  ██║   ██║██║   ██║   ██║       ██╔═██╔═╝
+███████╗██║  ██║   ██║   ╚██████╔╝╚██████╔╝   ██║       ██████║  
+╚══════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝  ╚═════╝    ╚═╝       ╚═════╝  
+
+██████╗  █████╗  ██████╗ ███████╗███████╗                        
+██╔══██╗██╔══██╗██╔════╝ ██╔════╝██╔════╝                        
+██████╔╝███████║██║  ███╗█████╗  ███████╗                        
+██╔═══╝ ██╔══██║██║   ██║██╔══╝  ╚════██║                        
+██║     ██║  ██║╚██████╔╝███████╗███████║                        
+╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝                        
+*/
+import Index from './pages/Index';
+
+const history = createBrowserHistory();
+
+const OpenRoute = ({ component: Component, ...rest }) => (
+	<Route
+		{...rest}
+		render={(props) => (
+			<Layout>
+				<Component {...props} />
+			</Layout>
+		)}
+	/>
+);
 
 function App() {
-	return <h1>APP JS</h1>;
+	const location = useLocation();
+	return (
+		<AnimatePresence exitBeforeEnter>
+			<Switch key={location.pathname} location={location}>
+				<OpenRoute history={history} exact path="/" component={Index} />
+			</Switch>
+		</AnimatePresence>
+	);
 }
 
 export default App;
